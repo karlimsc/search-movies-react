9b6adbcbc0d36117277d639780afc253482eca2d
karlimsc <karlianamsuarez@gmail.com>
2020-09-17 19:22:16 -0400

    proyecto listo

--- src/App.js
+++ src/App.js
@@ -1,21 +1,32 @@
 import React, {Component} from 'react';
 import {Title} from './components/Title.js'
 import {SearchForm} from './components/SearchForm.js'
+import { MoviesList }  from './components/MoviesList.js'
+
 import './App.css';
 import 'bulma/css/bulma.css'
 
 class App extends Component {
+  state = { results:  [] }
+
+  _handleResults = (results) =>  {
+    this.setState({results})
+  }
+
   render()
   {
     return (
       <div className="App">
       <Title>Proyecto Karli: Search Movies</Title>
         <div className='SearchForm-wrapper'>
-          <SearchForm/>
+          <SearchForm onResults={this._handleResults} />
         </div>
-
+        {this.state.results.length === 0
+           ?  <p>Sin resultados </p>
+           : <MoviesList movies={this.state.results}/>
+        }
       </div>
-    )
+    );
   }
 }
 
--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,36 +1,44 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react';
+
+const API_KEY = '60477527'
 
 export class SearchForm extends Component {
-  state= {
-    inputMovie: ''
+  state = { inputMovie:'' }
+
+  _handleChange = (e) => {
+    this.setState({inputMovie: e.target.value })
+
   }
 
-  _handleChange= (e) => {
-    this.setState({inputMovie: e.target.value})
-  }
-
-  _handleSubmit= (e) => {
+  _handleSubmit = (e) => {
     e.preventDefault()
-    alert(this.state.inputMovie)
+    const {inputMovie} = this.state
+
+    fetch(`http://www.omdbapi.com/?i=tt3896198&apikey=${API_KEY}&s=${inputMovie}`)
+    .then(res =>  res.json())
+    .then(results => {
+      const {Search, totalResults } = results
+      console.log(results)
+      this.props.onResults(Search)
+    })
   }
 
-  render() {
+  render(){
     return (
-      <form onSubmit= {this._handleSubmit} >
-      <div className="field has-addons">
-        <div className="control">
-          <input className="input"
-          onChange={this._handleChange}
-          type="text"
-          placeholder="Find a movie"/>
-        </div>
-        <div className="control">
-          <button className="button is-success is-light">
-            Search
-          </button>
-        </div>
-      </div>
-      </form>
+      <form onSubmit={this._handleSubmit}>
+        <div className="field has-addons">
+          <div className="control">
+              <input className="input"
+              onChange={this._handleChange}
+              type="text"
+              placeholder="Search a movie"/>
+            </div>
+          <div className="control">
+          <button className="button is-success is-light">Search</button>
+          </div>
+         </div>
+       </form>
     )
   }
+
 }
